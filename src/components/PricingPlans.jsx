167c65d38658dcd8c181d6eee0949fede8355ee4
mijankarim <mkatbd@gmail.com>
2024-05-11 19:21:31 +0100

    updated read me

--- src/components/PricingPlans.jsx
+++ src/components/PricingPlans.jsx
@@ -2,7 +2,7 @@ import PricingPlan from "./PricingPlan";
 
 const PricingPlans = ({ data, billingPeriod }) => {
   return (
-    <div className="gap-8 mt-2 md:mt-14 grid grid-cols-4 md:grid-cols-6 lg:grid-cols-12">
+    <div className="gap-8 mt-2 md:mt-6 grid grid-cols-4 md:grid-cols-6 lg:grid-cols-12">
       {data[`${billingPeriod}`]?.map((item, index) => (
         <PricingPlan key={index} item={item} />
       ))}
